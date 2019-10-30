f1ea5a88831650c3f37c3f4e72dfc673389c672c
Marta Sousa <marta.sousa@gmail.com>
2019-10-30 15:34:52 -0300

    inciando função de edição de produto

---
+++ editProduct.php
@@ -0,0 +1,69 @@
+<?php 
+
+  include('variables.php');
+
+  //Pegando o valor do ID do produto recebido:
+  $productID = $_GET['productID'];
+  // var_dump($_GET);
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Editar Produto</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+  
+<main>
+  <!-- Formulário de Alteração de Produtos -->
+  <div class="row justify-content-center">
+    <form class="col-4 form-input" method="post" action="" enctype="multipart/form-data">
+      <h1>Editar Produto</h1>
+      <div class="form-group">
+        <label for="productName">Nome</label>
+        <input type="text" name="productName" class="form-control" id="productName" required placeholder="Insira o nome do produto">
+      </div>
+      <div class="form-group">
+        <label for="productCategory">Categoria</label>
+        <!-- Categoria de produtos dinâmica -->
+        <select class="form-control" name="productCategory" id="productCategory">
+          <option value="select" selected disabled>Selecione uma categoria</option>
+          <?php
+            foreach ($_SESSION['productCategoryList'] as $category) { ?>
+            <option value="<?= $category ?>"><?= $category ?></option>    
+          <?php } ?>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="productDescription">Descrição</label>
+        <textarea class="form-control" name="productDescription" id="productDescription" cols="30" rows="3"
+          required></textarea>
+      </div>
+      <div class="form-group">
+        <label for="productQuantity">Quantidade</label>
+        <input type="number" name="productQuantity" class="form-control" id="productQuantity" placeholder="Insira a quantidade de produtos em estoque" required>
+      </div>
+      <div class="form-group">
+        <label for="productPrice">Preço</label>
+        <input type="number" name="productPrice" id="productPrice" class="form-control" required>
+      </div>
+      <div class="form-group">
+        <label for="productImage">Foto do Produto</label>
+        <input type="file" name="productImage" id="productImage" required>
+      </div>
+      <div class="d-flex justify-content-end">
+        <button type="submit" class="btn btn-primary">Enviar</button>
+      </div>
+    </form>
+  </div>
+</main>
+
+</body>
+</html>
